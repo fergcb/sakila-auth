5ac664aa53dc41aad615045c9fa79707a02d138b
fergcb <fergus.bentley@gmail.com>
2022-08-12 11:39:54 +0100

    Use System.getenv in production

--- src/main/java/uk/fergcb/sakila/auth/jwt/JwtKeys.java
+++ src/main/java/uk/fergcb/sakila/auth/jwt/JwtKeys.java
@@ -1,14 +1,21 @@
 package uk.fergcb.sakila.auth.jwt;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvException;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
 import java.security.Key;
 
 public class JwtKeys {
+    private static final String VAR_NAME = "JWT_PRIVATE_KEY";
     protected static Key getPrivateKey() {
-        String encoded = Dotenv.load().get("JWT_PRIVATE_KEY");
+        String encoded;
+        try {
+            encoded = Dotenv.configure().load().get(VAR_NAME);
+        } catch (DotenvException ex) {
+            encoded = System.getenv(VAR_NAME);
+        }
         byte[] decoded = Decoders.BASE64.decode(encoded);
         return Keys.hmacShaKeyFor(decoded);
     }
