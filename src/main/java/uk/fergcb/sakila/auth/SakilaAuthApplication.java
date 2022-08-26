85207aa0f184ba38276ee8892c6db597e8a381c5
fergcb <fergus.bentley@gmail.com>
2022-08-26 08:35:05 +0100

    Add CORS config

--- src/main/java/uk/fergcb/sakila/auth/SakilaAuthApplication.java
+++ src/main/java/uk/fergcb/sakila/auth/SakilaAuthApplication.java
@@ -1,15 +1,27 @@
 package uk.fergcb.sakila.auth;
 
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SakilaAuthApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SakilaAuthApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("https://sakila.fergcb.uk/");
+			}
+		};
 	}
 
 }
